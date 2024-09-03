bd0a589a2edb0a596106cc22c1bb15c486157228
sandikodev <androxoss@hotmail.com>
2024-09-03 18:48:42 +0700

    give base_url example in practice

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://macbook-pro.local/gudang_app';
 
 /*
 |--------------------------------------------------------------------------
